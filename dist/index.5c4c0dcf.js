dd1751c8602b644d716681e43b512a5e3bc04432
Nana Kwakwa <nanakwakwa@gmail.com>
2023-06-18 13:46:37 -0400

    production deployment code ready

---
+++ dist/index.5c4c0dcf.js
@@ -0,0 +1,2 @@
+$(function(){let o=document.querySelectorAll('[data-bs-toggle="popover"]');[...o].map(o=>new bootstrap.Popover(o)),$("#dealButton").click(function(){$("#dealModal").modal("show")}),$("#eventButton").click(function(){$("#eventModal").modal("show")})});
+//# sourceMappingURL=index.5c4c0dcf.js.map
